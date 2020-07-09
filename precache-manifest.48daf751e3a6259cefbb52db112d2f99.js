677269c84df6f1566eaa258dd0d7a9bc0a67c8df
Dwi Prastowo <dprastowo@gmail.com>
2020-07-10 05:12:44 +0700

    deploy redux

---
+++ precache-manifest.48daf751e3a6259cefbb52db112d2f99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "97059c84eca8f908e4fa",
+    "url": "/static/js/main.97059c84.chunk.js"
+  },
+  {
+    "revision": "2d03a5e01dd99ebe403f",
+    "url": "/static/js/1.2d03a5e0.chunk.js"
+  },
+  {
+    "revision": "97059c84eca8f908e4fa",
+    "url": "/static/css/main.38db1a0b.chunk.css"
+  },
+  {
+    "revision": "9e42dff86506acbffe7287ca29b9e81d",
+    "url": "/index.html"
+  }
+];
